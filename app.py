8b3c45beb9a59ce55c3b3e59434caa177e93b6a6
Zentaro <zentaro0824@gmail.com>
2022-06-19 19:09:37 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+import streamlit as st
+import os
+import pandas as pd
+
+st.title("デードリ分析")
+
+DIR_PATH = 'csvs'
+
+file = st.file_uploader('Upload your csv file', type=['csv'])
+if file:
+    st.markdown(f'{file.name} has been uploaded')
+    dir_path = os.path.join(DIR_PATH, file.name)
+    # 画像を保存する
+    with open(dir_path, 'wb') as f:
+        f.write(file.read())
+        
+    # 保存した画像を表示
+    img = pd.read_csv(dir_path)
+    st.write(img)
+
+if file is not None:
+    
+	keisu = st.slider('反発係数', 0.0, 2.5, 0.7)
+
+	H=nx.Graph()   # use ne
+
+	csv_file = open(dir_path, "r", encoding="UTF-8", errors="", newline="" )
+	#リスト形式
+	f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+
+	for row in f:
+	# weight付でnodeとエッジも入れる
+		H.add_weighted_edges_from([(row[0], row[1], float(row[2]))])
+
+	# weightによって線の太さを変える
+	edge_width = [d["weight"] * 5 for (u, v, d) in H.edges(data=True)]
+
+	# posを定義
+	pos = nx.spring_layout(H, k=keisu)  # k = node間反発係数
+
+	# 図示
+	nx.draw(H,pos, with_labels=True, width=edge_width)
+	plt.savefig("exported.png")
+
+if st.button("分析開始"):
+    st.image("exported.png")
+
+with open("exported.png", "rb") as file:
+    st.download_button(
+        label="Download data as image",
+        data=file,
+        file_name='exported.png',
+        mime='',
+    )
